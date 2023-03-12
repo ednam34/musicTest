6ab5b26005ad15989dae4bbc9f6b3bfb1a5cf2fb
KAABECHE rayane <rayanoo1302@gmail.com>
2023-03-12 22:28:35 +0100

    je taime la

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const { appendFile } = require('fs');
-const http = require('http');
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
 
-const hostname = '51.210.149.60'; // Remplacez cette valeur par l'adresse IP de votre serveur
+const app = express();
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Je t'aime Clea\n");
-});
+// Définit le dossier contenant les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(port, hostname, () => {
-  console.log(`Le serveur fonctionne sur http://${hostname}:${port}/`);
-});
-
-
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
-    //go to index.html
-    res.sendFile(path.join(__dirname, 'index.html'));
-    })
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Lance le serveur
+app.listen(port, () => {
+  console.log(`Le serveur fonctionne sur le port ${port}`);
+});
