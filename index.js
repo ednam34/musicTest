cbb21e3a51745a841b1803081401b5a14c431aa6
KAABECHE rayane <rayanoo1302@gmail.com>
2023-03-13 15:33:22 +0100

    test

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const port = 80;
 const hostname = '51.210.149.60';
 
 // Définit le dossier contenant les fichiers statiques
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Route pour la page d'accueil
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
 // Lance le serveur
 app.listen(port,hostname, () => {
   console.log(`Le serveur fonctionne sur le port ${port}`);
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
