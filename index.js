c2ecd4eb2f0e273dee836107eafb09c55a2b9157
KAABECHE rayane <rayanoo1302@gmail.com>
2023-03-13 12:45:34 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = 80;
 
 // Définit le dossier contenant les fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
