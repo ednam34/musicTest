9e5d3ad569f7e07f3b114220bb5b42e13deebb77
KAABECHE rayane <rayanoo1302@gmail.com>
2023-03-13 15:36:26 +0100

    test

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const path = require('path');
-
+const http = require('http');
 const app = express();
 const port = 80;
 const hostname = '51.210.149.60';
 
 // Définit le dossier contenant les fichiers statiques
 
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
 
 // Lance le serveur
 app.listen(port,hostname, () => {
